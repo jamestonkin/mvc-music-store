1c788296be8747e421623f16e6b90498f531997f
James W. Tonkin <btonkin71@comcast.net>
2017-07-28 10:31:48 -0500

    Created some models, Artist, Album, Review.

---
+++ MvcMusicStore/MvcMusicStore/Models/Review.cs
@@ -0,0 +1,14 @@
+﻿namespace MvcMusicStore.Models
+{
+    public class Review
+    {
+        public int ReviewID { get; set; }
+
+        public int AlbumID { get; set; }
+        public virtual Album Album { get; set; }
+
+        public string Contents { get; set; }
+
+        public string ReviewerEmail { get; set; }
+    }
+}
